99965255baf2fa794853ab28e9917c5174278080
diegoarcos7 <arcos.5c@gmail.com>
2021-11-02 18:41:00 -0500

    Tailwin and JS configurado

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    content: ['./public/**/*.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        lora: "'Lora', serif",
+        dm: "'DM mono', monoscape",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
